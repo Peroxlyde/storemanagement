5058a3025fbb6dcfed55d9dfd3ccf3ed993fce50
Picha Lerttrakul <74087752+Plerttra@users.noreply.github.com>
2024-11-24 05:48:33 +0700

    Delete Admin directory

--- Admin/quantity_handling.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <body>
-        <?php 
-        echo "Welcome: <br>";
-        echo "Product ID:" .$_GET['productid']. "<br>";    
-        echo "Quantity:" .$_GET['quantity']. "<br>";    
-        ?>
-    </body>
-</html>
